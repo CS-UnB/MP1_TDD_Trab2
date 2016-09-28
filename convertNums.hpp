a733582cfdb7daf2568eb15714434f7fda148dc4
ottok92 <ottok92@gmail.com>
2016-09-28 17:25:32 -0300

    New commenting and asserts
    New header file containing asserts. Also better structured

--- convertNums.hpp
+++ convertNums.hpp
@@ -22,4 +22,65 @@ class ConvertNums {
     int VERIFY_Subtraction(string & String_toVerify);
 };
 
+/***************************************************************************
+* Função: Converte numerais romanos em arabicos
+* Descrição
+*   Recebe uma string de tamanho maximo 30 characteres(29 numerais)
+*   e retorna um valor inteiro equivalente.
+*   Função cliente de todas as outras.
+*   Verifica regras de formação de numerais romanos.
+*   De acordo com tais regras, soma ou subtrai cada valor equivalente.
+*   ao caracter corrente. Executa do fim para o inicio da string.
+*
+* Parâmetros
+*   romanNumeral_String - String a ser convertida.
+* Valor retornado
+*   Total do somatorio(ou subtracao) de todos os numerais romanos.
+* Assertiva de entrada
+*   romanNumeral_String != NULL
+*   dimensao( romanNumeral_String ) < 30
+* Assertiva de saida
+*
+***************************************************************************/
+int ConvertNums::convert_toArabic(string & romanNumeral_String);
+
+/***************************************************************************
+* Função: Converter long para ASCII
+* Descrição
+*
+ Converte um inteiro long para um string ASCII.
+*
+ O string resultado estará alinhado à esquerda no buffer de dimASCII
+*
+ caracteres fornecido
+* Parâmetros
+*
+ dimASCII - número máximo de caracteres do string inclusive
+*
+ o caractere zero terminal.
+*
+ pNumASCII - ponteiro para o espaço que receberá o string.
+*
+ Será truncado à direita caso o string convertido
+*
+ exceda a dimensão limite. O primeiro caractere
+*
+ será ‘-‘ se e somente se número < 0
+*
+ Numero
+ - inteiro a ser convertido para string
+* Valor retornado
+*
+ veja as declarações das condições de retorno
+* Assertiva de entrada
+*
+ pNumASCII != NULL
+*
+ dimensao( *pNumASCII ) >= dimASCII
+*
+ dimASCII >= max( 3 , 2 + log10( abs( Numero ))
+ que tal 12?
+***************************************************************************/
+int ConvertNums::getRomanEquivalentValue(char  romanString);
+
 #endif /* CONVERTNUMS_HPP_ */
